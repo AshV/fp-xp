ca1a961adbd85e013f91f70e0f2e21f69dfb58c2
AshV <ashish6318@gmail.com>
2017-12-17 14:07:59 +0530

    Add C# Project

---
+++ C-Sharp_AshV/XP_FP_Workshop/MedicalItems.cs
@@ -0,0 +1,10 @@
+﻿namespace XP_FP_Workshop
+{
+    public class MedicalItems
+    {
+        public readonly string Paracetamol;
+        public readonly string Ibuprofen;
+        public readonly string Syringe;
+        public readonly string Scalpel;
+    }
+}
